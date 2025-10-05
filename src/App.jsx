7056ace79a1ebd7a5b66c11071d92e96ae290b4f
Julian Enrique <julianbuitragocharry2006@gmail.com>
2025-10-05 01:14:02 -0500

    add map openseadragon

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,17 @@ import './App.css'
 import GalleryMissions from './Pages/GalleryMissions';
 
 function AppLayout() {
+  const tilesUrl = "https://data.lroc.im-ldi.com/ptif/zoomify/ser/estore/lroc/web/LRO-L-LROC-3-CDR-V1.0/LROLRC_1063C/EXTRAS/BROWSE/2025166/M1504671121RC_pyr.tif/";
+  const width = 2532; // line simples
+  const height = 52224; // images lines
+  
   return (
     <>
       <Navbar />
       <Routes>
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/missions" element={<GalleryMissions />}/>
-        <Route paht="/test" element={<Map/>} />
+        <Route path="/test" element={<Map tilesUrl={tilesUrl} width={width} height={height} />} />
       </Routes>
     </>
   );
--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,7 +1,23 @@
-const Map = () => {
-  return (
-    <div>Map</div>
-  )
-}
+import './Styles/Map.css'
+import { useMemo } from "react";
 
-export default Map
+const Map = ({ tilesUrl, width, height }) => {
+  const iframeSrc = useMemo(() => {
+    const params = new URLSearchParams({ tilesUrl, width, height });
+    return `/map.html?${params.toString()}`;
+  }, []);
+
+  return (
+    <div className='container-iframe'>
+      <h2>Titulo</h2>
+      <iframe
+        src={iframeSrc}
+        style={{ border: "none" }}
+        title="Map"
+        />
+      <p>Description...</p>
+    </div>
+  );
+};
+
+export default Map;
